2c84416cb4f51b202611e33af82504df9dc64001
OpenShift guest <hugo102@gmail.com>
2013-04-18 00:17:55 -0400

    “number3.1”

--- app.js
+++ app.js
@@ -161,19 +161,5 @@ socketIo.on('connection', function(socket) {
 
 });
 
-/*
-io.sockets.on('connection', function (socket) {
-  var tweets = setInterval(function () {
-    getBieberTweet(function (tweet) {
-      socket.volatile.emit('bieber tweet', tweet);
-    });
-  }, 100);
-
-  socket.on('disconnect', function () {
-    clearInterval(tweets);
-  });
-});
-*/
-
 
 
